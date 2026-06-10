bf287591cc8422f884969fe68f653ed9732f6e3e
dsagm7 <dsagm7>
2026-06-10 14:51:12 +0530

    default webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const config = {
+  // serves as entry point to the application. If we do not provide
+  // exact file name webpack will assume the file is index.js e.g './src' will be treated as './src/index.js'
+  entry: "./src",
+  // the config for output
+  output: {
+    path: path.resolve(__dirname, "dist"), // dist folder is default if not specified
+    filename: "main.js", // main.js is default if not provided
+  },
+  mode: "production", // production is default if not provided
+};
+
+module.exports = config;
