0e796e8bab451a2f72cdf98ec9d6044d30218aeb
dsagm7 <dsagm7>
2026-06-26 12:21:53 +0530

    17. Optimize css for prod

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,11 +1,25 @@
 const common = require("./webpack.common.config");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CSSMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = merge(common, {
   output: {
     filename: "js/[name].[contenthash:12].js",
   },
   mode: "production",
+  // special optimization for CSS
+  // this uses CSSNano tool under the hood to optimize css for prod
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: ["default", { discardComments: { removeAll: true } }],
+        },
+      }),
+    ],
+  },
   module: {
     rules: [
       {
